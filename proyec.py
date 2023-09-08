a5538aeed4bbddd6012ac880a34297b0d61640c4
Miguel Cordoba <miguelcordoba.ds@gmail.com>
2023-09-08 11:09:18 -0500

    ajuste al proyecto

--- proyec.py
+++ proyec.py
@@ -12,7 +12,10 @@ def run():
         actividades.update({y:z})
 
     min_total = sum(actividades.values())
-    print(min_total)
+    print("\nResumen de activiades: \n")
+    for clave, valor in actividades.items():
+        print(f"{clave} lo hiciste por {valor} minutos")
+    print(f"\nTiempo total en actividades utiles = {min_total} minutos ({min_total/60} horas)")
     fun.criterio(min_total) 
 
 if __name__ == "__main__":
